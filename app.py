6904c8994c3c0be9374b75968fcd52a55d6da74c
tammysams <37048195+tammysams@users.noreply.github.com>
2021-01-31 20:29:52 -0800

    Adds base flask app with validation handler

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+from webargs.flaskparser import parser, abort
+
+app = Flask(__name__)
+api = Api(app)
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Housekeeping Clean Prediction Aggregate',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/schema/',
+    'APISPEC_SWAGGER_UI_URL': '/docs/'
+})
+docs = FlaskApiSpec(app)
+
+@parser.error_handler
+def handle_request_validation_error(err, req, schema, error_status_code, error_headers):
+    abort(400, errors=err.messages)
+
