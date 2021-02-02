a39275fcbe2526bbf3dbdb234fcfedf4e6e4d1b1
tammysams <37048195+tammysams@users.noreply.github.com>
2021-02-02 12:13:36 -0800

    merge conflict

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ from flask import Flask, Blueprint, jsonify
 from flask_restful import Api
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
-<<<<<<< HEAD
 from src.extension import PatchedFlaskApiSpec as FlaskApiSpec
 
-=======
-from flask_apispec.extension import FlaskApiSpec
->>>>>>> main
 from webargs.flaskparser import parser, abort
 from src.endpoints import CleanPredictionsAPI
 
@@ -25,10 +21,6 @@ app.config.update({
     'APISPEC_SWAGGER_URL': '/schema/',
     'APISPEC_SWAGGER_UI_URL': '/docs/'
 })
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 docs = FlaskApiSpec(app)
 
 @app.errorhandler(404)
